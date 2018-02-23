a3fdd64b6cdfb72391a04bb54fdc098c2ac1211f
Hamed Montazeri <hamed.montazeri@mhp.com>
2018-02-23 15:53:33 -0500

    eslint and sass support added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="box">
+            <h1>Hamed Montazeri</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
