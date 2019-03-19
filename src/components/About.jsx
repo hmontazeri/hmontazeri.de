eb5f9db8639b24954fea0287f495860765cfcf6e
Hamed Montazeri <hamed.montazeri@mhp.com>
2019-03-19 14:42:37 -0400

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,33 +4,85 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>Hamed Montazeri</h1>
-        <p>
-          Currently working for <a href="http://mhp.com">MHP - A Porsche Company</a> in Atlanta,
-          Georgia as a Senior Software Engineer / Senior Consultant. I do architecure, write code
-          and lead a team.
-        </p>
-        <p>
-          Startet coding <b>2008</b> in Schwaebisch Hall, Germany. University of Heilbronn tought me{' '}
-          <b>Java</b>. Fell in Love with script languages. Did <b>Rails</b>, found and got addicted
-          to <b>JavaScript / NodeJS</b>.
-        </p>
-        <p>
-          Ever since I did a lot of projects with <b>Java (Spring)</b>, <b>Rails</b> and{' '}
-          <b>AngularJS</b>. Now focused on <b>Serverless (λ)</b> APIs with{' '}
-          <b>React / React Native</b>.
-        </p>
-        <p className="links">
-          <a href="https://github.com/hmontazeri">
-            <span className="typcn typcn-social-github-circular" />
-          </a>
-          <a href="https://linkedin.com/in/hmontazeriger">
-            <span className="typcn typcn-social-linkedin-circular" />
-          </a>
-          <a href="https://twitter.com/h_montazeri">
-            <span className="typcn typcn-social-twitter-circular" />
-          </a>
-        </p>
+        <ul>
+          <li className="item">
+            <div className="prefix pull-left">
+              <h1>01</h1>
+            </div>
+            <div className="content pull-left">
+              <h1>Hamed Montazeri</h1>
+              <p>
+                I'm a Software Engineer with over 5 years experience in the
+                field building applications.
+              </p>
+            </div>
+            <div className="clearfix" />
+          </li>
+
+          <li className="item">
+            <div className="prefix pull-left">
+              <h2>02</h2>
+            </div>
+            <div className="content pull-left">
+              <h2>Skill Set</h2>
+              <div className="33-3">
+                <ul>
+                  <li>
+                    <span className="typcn typcn-scissors" /> Solution Architect
+                  </li>
+                  <li>
+                    <span className="typcn typcn-code" /> Software Development
+                  </li>
+                  <li>
+                    <span className="typcn typcn-weather-cloudy" /> Cloud
+                    Architect
+                  </li>
+                  <li>
+                    <span className="typcn typcn-brush" /> UI/UX Design
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="clearfix" />
+          </li>
+
+          <li className="item">
+            <div className="prefix pull-left">
+              <h3>03</h3>
+            </div>
+            <div className="content pull-left">
+              <h3>Technical Skill</h3>
+              <div className="33-3">
+                <ul>
+                  <li>Node / React</li>
+                  <li>Java / Spring / Kotlin / Android</li>
+                  <li>Ruby on Rails</li>
+                  <li>Swift / iOS</li>
+                  <li>SQL / NoSQL</li>
+                  <li>AWS / GCloud</li>
+                </ul>
+              </div>
+            </div>
+            <div className="clearfix" />
+          </li>
+
+          <li className="item">
+            <div className="prefix pull-left">
+              <h4>04</h4>
+            </div>
+            <div className="content pull-left">
+              <h4>Education</h4>
+              <div className="33-3">
+                <p className="pull-left">
+                  2015 - Bachelor of Science <br /> Hochschule Heilbronn,
+                  Germany
+                </p>
+              </div>
+              <div className="clearfix" />
+            </div>
+            <div className="clearfix" />
+          </li>
+        </ul>
       </div>
     );
   }
--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import hamed from '../images/hamed.jpg';
 
-const Picture = () => <div className="picture" />;
+const Picture = () => (
+  <div className="picture">
+    <div className="picture-wrapper">
+      <img src={hamed} alt="Hamed Montazeri" />
+    </div>
+  </div>
+);
 
 export default Picture;
